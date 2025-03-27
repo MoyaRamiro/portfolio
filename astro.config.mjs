a310a64f8f28e3210afc41af83ea023903bde9b1
MoyaRamiro <moya04.ramiro@gmail.com>
2025-03-27 19:42:24 -0300

    Add sitemap integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   vite: {
     optimizeDeps: {
       include: ["zwitch"],
